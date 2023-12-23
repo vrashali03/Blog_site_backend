f72a76b01bb7bc5daa2c2fc7b074220d0eed64b6
Vrashali Naitam <vrushali2214@gmail.com>
2023-12-23 22:01:01 +0530

    Changed path of uplaod

--- controller/controller.routes.js
+++ controller/controller.routes.js
@@ -5,7 +5,8 @@ const app = express();
 
 const route = express.Router();
 
-const destination = "/opt/render/project/src/uploads/";
+// const destination = "/opt/render/project/src/uploads/";
+const destination = path.join(__dirname, "../../../../uploads");
 const upload = multer({ dest: destination });
 
 //All the imports are here
