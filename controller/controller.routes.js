2b7da83b7fb9a644c02822b527dceb8d16f6e901
Vrashali Naitam <vrushali2214@gmail.com>
2023-12-24 16:58:00 +0530

    correcting path

--- controller/controller.routes.js
+++ controller/controller.routes.js
@@ -5,7 +5,7 @@ const app = express();
 
 const route = express.Router();
 
-const destination = "/opt/render/project/src/";
+const destination = path.join(__dirname, "../uploads/");
 const upload = multer({ dest: destination });
 
 //All the imports are here
